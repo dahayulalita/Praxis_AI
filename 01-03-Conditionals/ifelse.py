e07e3b83d7e770e716b5206700118c6ef37ec027
dahayulalita <dahayulalita139@gmail.com>
2024-07-31 11:04:51 +0700

    new lesson

---
+++ 01-03-Conditionals/ifelse.py
@@ -0,0 +1,11 @@
+user = "Soobin"
+user2 = "Yeonjun"
+
+user_in = input("Input the user name: ")
+
+if user_in == user: # kondisi pengecekan
+    print("Welcome!!")
+elif user_in == user2:
+    print("Welcome Bro!!")
+else:
+    print("Who are you??")
---
+++ 01-03-Conditionals/readme.md
@@ -0,0 +1,6 @@
+## Multi-Purpose Calculator
+This is a code for a simple multi-purpose calculator
+## Algorithm
+1. The app shows welcome display
+2. The app asks for user's option
+3. If (addition/substraction/multiplies/division)
